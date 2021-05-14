81e32064a9f0b3c664653f73cea37c6388d844e5
Adam Kiss <a.kiss1994@gmail.com>
2021-05-14 12:05:49 +0100

    Adding voting logic

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import './JokeList.css';
+import "./JokeList.css";
+import Joke from "./Joke";
+import uuid from "uuid/dist/v4";
 import React, { Component } from "react";
 
 class JokeList extends Component {
@@ -17,9 +19,16 @@ class JokeList extends Component {
             let res = await axios.get("https://icanhazdadjoke.com/", {
                 headers: { Accept: "application/JSON" },
             });
-            jokes.push(res.data.joke);
+            jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
         }
         this.setState({ jokes: jokes });
+    }
+    handleVote(id, delta) {
+        this.setState((st) => ({
+            jokes: st.jokes.map((j) =>
+                j.id === id ? { ...j, votes: j.votes + delta } : j
+            ),
+        }));
     }
     render() {
         return (
@@ -28,12 +37,17 @@ class JokeList extends Component {
                     <h1 className="JokeList-title">
                         <span>Dad</span> Jokes
                     </h1>
-                    <img src="https://www.creativefabrica.com/wp-content/uploads/2019/03/Dad-joke-loading.jpg"/>
-                    <button className='JokeList-getmore'>New Jokes</button>
-                    </div>
+                    <img src="https://www.creativefabrica.com/wp-content/uploads/2019/03/Dad-joke-loading.jpg" />
+                    <button className="JokeList-getmore">New Jokes</button>
+                </div>
                 <div className="JokeList-jokes">
                     {this.state.jokes.map((j) => (
-                        <div>{j}</div>
+                        <Joke
+                            votes={j.votes}
+                            text={j.text}
+                            upvote={() => this.handleVote(j.id, 1)}
+                            downvote={() => this.handleVote(j.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
